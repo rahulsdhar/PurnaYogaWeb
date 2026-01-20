e72f553af46d424d8343bc9893a530377603cdf0
Rahul Sutradhar <rahulsdhar51@gmail.com>
2026-01-20 17:39:22 +0530

    SnapScrollFeatureAdded

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+scrollPad=20;
 // Smooth scroll for internal links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -14,3 +15,56 @@ document.addEventListener('contextmenu', (e) => {
     if(cn!="copyit")
         e.preventDefault()
 });
+var currentScrollY=0;
+window.addEventListener('scroll', () => {
+    const isScrollDown=window.scrollY>currentScrollY
+    currentScrollY=window.scrollY;
+    const currSecIndex=getCurrentSectionIndex();
+    if(isScrollDown){
+        if(isSectionScrollDownEnd()){
+            gotoSection(currSecIndex+1);
+        }
+    }else{
+        if(isSectionScrollTopEnd()){
+            gotoSection(currSecIndex-1);
+        }
+    }
+});
+
+window.addEventListener('DOMContentLoaded',init)
+
+function init(){
+    sectionDOMs=document.getElementsByTagName("section");
+}
+
+var sectionDOMs;
+
+function isSectionScrollDownEnd(){
+    const i=getCurrentSectionIndex();
+    const c=sectionDOMs[i];
+    return window.innerHeight+currentScrollY>c.offsetTop+c.offsetHeight+scrollPad;
+}
+
+function isSectionScrollTopEnd(){
+    const i = getCurrentSectionIndex();
+    if (i <= 0) return false; // no section above
+    const c = sectionDOMs[i];
+    console.log(currentScrollY - (c.offsetTop - scrollPad));
+    return currentScrollY <= c.offsetTop - scrollPad;
+}
+
+
+function getCurrentSectionIndex(){
+    let i;
+    for(i=0;i<sectionDOMs.length;i++){
+        const element=sectionDOMs[i];
+        const h=element.offsetTop;
+        if(h>currentScrollY+window.innerHeight/2){
+            return i-1;
+        }
+    }
+}
+
+function gotoSection(i){
+    sectionDOMs[i].scrollIntoView({ behavior: 'smooth' });
+}
