5d89d349a9486ce59e09344705f91f7791a1350a
Rahul Sutradhar <rahulsdhar51@gmail.com>
2026-01-24 21:07:00 +0530

    Video and Form Update

--- script.js
+++ script.js
@@ -14,8 +14,47 @@ document.addEventListener('contextmenu', (e) => {
         e.preventDefault()
 });
 
-/* window.addEventListener('DOMContentLoaded',init)
+const PriceSelAmt=[10000,2999,150];
+
+window.addEventListener('DOMContentLoaded',init)
+var CourseSelectDOM,PayPriceBtnAmtTextDOM,UPIDOM,UPIDBtnDOM;
+
+function setPriceAmt(amt) {
+    PayPriceBtnAmtTextDOM.innerText=AmtToStr(amt);
+    const upihref="upi://pay?pa=purnajitasen47@oksbi&pn=Purnajita%20Sen&cu=INR&am="+amt;
+    UPIDOM.setAttribute("href",upihref);
+    UPIDBtnDOM.setAttribute("href",upihref);
+}
+
+function AmtToStr(a){
+    let b=a+"";
+    let l=(b.length-3)/2;
+    let c="";
+    let i;
+    const isOdd=1-(b.length%2);
+    if(isOdd==1 && l>0){
+        c=b.charAt(0)+',';
+    }
+    for(i=isOdd;i<l;i++){
+        const d=b.substring(i*2,(i+1)*2);
+        c+=d+',';
+    }
+    c+=b.substring(b.length-3);
+    return c;
+}
+
+function selectCourse(i){
+    CourseSelectDOM.value=i;
+    setPriceAmt(PriceSelAmt[i]);
+}
 
 function init(){
-    
-} */
+    CourseSelectDOM=document.getElementById("prodSel");
+    PayPriceBtnAmtTextDOM=document.getElementById("joinFeesAmtSpan");
+    UPIDOM=document.getElementById("upiID");
+    UPIDBtnDOM=document.getElementById("upiIDbtn")
+    CourseSelectDOM.addEventListener("change",()=>{
+        setPriceAmt(PriceSelAmt[CourseSelectDOM.value]);
+    });
+    selectCourse(0);
+}
